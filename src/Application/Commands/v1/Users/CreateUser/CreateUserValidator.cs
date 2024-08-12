198dbb51586b5dc5142905ae3f89533308ef963a
Alexandre Betassa de Souza <5by5.betassa@voeazul.com.br>
2024-08-12 14:14:04 -0300

    Adicionar arquivos de projeto.

---
+++ src/Application/Commands/v1/Users/CreateUser/CreateUserValidator.cs
@@ -0,0 +1,8 @@
+﻿using FluentValidation;
+
+namespace Autenticacao.Jwt.Application.Commands.v1.Users.CreateUser
+{
+    public class CreateUserValidator : AbstractValidator<CreateUserCommand>
+    {
+    }
+}
