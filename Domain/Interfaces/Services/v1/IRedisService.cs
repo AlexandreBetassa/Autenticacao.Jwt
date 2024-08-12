12177eea936e18c7556a062eb80f26a2999a7dd8
Alexandre Betassa de Souza <5by5.betassa@voeazul.com.br>
2024-08-12 14:14:04 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Interfaces/Services/v1/IRedisService.cs
@@ -0,0 +1,8 @@
+﻿namespace Autenticacao.Jwt.Domain.Interfaces.Services.v1
+{
+    public interface IRedisService
+    {
+        Task CreateAsync(string key, object data, int expirationInMinutes = 15);
+        Task<string> GetKey(string key);
+    }
+}
